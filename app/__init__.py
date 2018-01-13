791e7b29fe1d94f44db9e77df83e2ddc29c7d019
Evgeniy Ovchinnikov <khatangatao@gmail.com>
2018-01-13 21:12:20 +0700

    Начало создания базы данных и модуля миграции

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,14 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
+# создаем объект db для предствления БД
+db = SQLAlchemy(app)
+# создаем объект migrate для представления механизма миграции БД
+migrate = Migrate(app, db)
 
-from app import routes
+from app import routes, models
